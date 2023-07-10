3298bc1e635206d095797c3d3fabe6fbe5b30d85
Vaibhav Srivastava <81919583+Vaibhav832@users.noreply.github.com>
2023-07-10 21:06:07 +0530

    Add files via upload

---
+++ Mock Test 2/DOM Manipulator/assets/script.js
@@ -0,0 +1,7 @@
+const box = document.getElementById('box');
+
+box.addEventListener('click', function() {
+  box.style.backgroundColor = 'yellow';
+  box.style.width = '600px';
+  box.style.height = '600px';
+});
